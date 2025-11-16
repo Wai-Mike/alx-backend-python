7a70b2c9f7bb80fb2c8b59b546eaef0c69d10d34
Wai Mike <waimichael35@gmail.com>
2025-11-16 21:12:17 +0200

    Add authentication and permission classes to REST_FRAMEWORK settings

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -121,6 +121,12 @@ AUTH_USER_MODEL = 'chats.User'
 
 # REST Framework settings
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
